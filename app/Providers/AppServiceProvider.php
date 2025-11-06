5eba5d0d002dec997be32bc7f5f050524db00d73
Arthur Monney <monneylobe@gmail.com>
2025-11-06 09:42:27 +0100

    Initial commit

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Providers;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ServiceProvider;
+
+final class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        $this->bootModelsDefaults();
+    }
+
+    private function bootModelsDefaults(): void
+    {
+        Model::unguard();
+    }
+}
